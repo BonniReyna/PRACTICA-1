96008121892f7359c95b8524c0ab086d893b6598
BONNI VR <bonni_pr13@outlook.com>
2025-02-03 16:39:12 -0600

    HICE EL EJERCICIO 4

--- src/js/Script1.js
+++ src/js/Script1.js
@@ -2,3 +2,13 @@ console.log("hola mundo")
 console.log("Vamos a hacer el segundo cambio")
 console.log("Vamos a hacer el xd cambio")
 
+// Declaración de variables
+let x = 10;
+let y = 5;
+let z = 2;
+
+// Cálculo 
+let resultado = (x + y) * z / 2;
+
+// Imprimir el resultado en la consola
+console.log("El resultado es:", resultado);
